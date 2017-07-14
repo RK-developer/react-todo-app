5dcb4150fce271840ebd9184a5c8d187473d87d9
mgmourogs@users.noreply.github.com <mgmourgos@users.noreply.github.com>
2017-07-13 23:37:20 -0700

    Cleaned up thunks

--- client/actions.js
+++ client/actions.js
@@ -10,13 +10,15 @@ export const addTodoPreFetch = (title) => ({
 
 export const addTodo = (title) => {
     return dispatch => {
+
+        //Perform expected result of the POST API request
         dispatch(addTodoPreFetch(title))
+
+        //API call to POST a new todo with {title}
         return fetch('/api/todos', {
             method: 'POST',
             body: JSON.stringify({ title: title}),
-            headers: {
-                "Content-Type": "application/json"
-            }
+            headers: { "Content-Type": "application/json" }
         })
         .then(
             response => response,
@@ -34,7 +36,11 @@ export const removeTodoPreFetch = (id) => ({
 
 export const removeTodo = (id) => {
     return dispatch => {
+
+        //Perform expected result of the DELETE API request
         dispatch(removeTodoPreFetch(id))
+
+        //API call to delete todo {id}
         return fetch('/api/todos/' + id, {
             method: 'DELETE'
         })
@@ -53,35 +59,33 @@ export const editTodoPreFetch = (id, title) => ({
 
 export const editTodo = (id, title) => {
     return dispatch => {
+
+        //Perform expected result of the PUT API request
         dispatch(editTodoPreFetch(id, title))
+
+        //API call to edit todo {id}, with new title: {title}
         return fetch('/api/todos/' + id, {
-            method: 'PUT',
-            body: JSON.stringify({ title: title, id: id}),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(
-            response => {
-                dispatch(fetchTodos())
-            },
-            error => console.log('An error occured.', error)
-        )
+                method: 'PUT',
+                body: JSON.stringify({ title: title, id: id}),
+                headers: { "Content-Type": "application/json" }
+            })
+            .then(
+                response => dispatch(fetchTodos()),
+                error => console.log('An error occured.', error)
+            )
     }
 }
 
+//A thunk which will fetch+set the state of the todo list from the backend db
 export const fetchTodos = () => {
     return dispatch => {
         return fetch('/api/todos')
             .then(
-                response => {
-                    return response.json()
-                },
+                response => response.json(),
                 error => console.log('An error occured.', error)
             )
             .then(
                 json => dispatch(setTodos(json))
-                //json => console.log(json)
             )
     }
 }
